088535ee6130c7738aa954ee8ff1fe8a28c78ae4
John Grenard <John@JohnGrenard.com>
2019-10-02 22:27:04 -0700

    Add TSK timer functionality

---
+++ TSK_timer.h
@@ -0,0 +1,49 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef TSK_TIMER_H
+#define	TSK_TIMER_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern uint24_t TSK_timer;
+    
+uint24_t TSK_timer_get();
+    
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* TSK_TIMER_H */
+
---
+++ tsk_timer.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+uint24_t TSK_timer;
+
+uint24_t TSK_timer_get(){
+    return(TSK_timer);
+}
+
